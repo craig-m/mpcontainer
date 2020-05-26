cd8ee7a49b87bb03222a3bae74744520ce39e555
craig <craig.martin@cloud212.com.au>
2020-05-27 01:01:43 +1000

    fixes

--- dev-vm/microk8s-setup.sh
+++ dev-vm/microk8s-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Setup MicroK8 https://microk8s.io/
+# ToDo: fix
 
 # confirm
 echo -e "\n Setup MicroK8 env? yes / no \n"
@@ -13,17 +14,21 @@ while true; do
     esac
 done
 
-sudo snap install microk8s --classic --channel=1.18/stable
-sudo microk8s status --wait-ready
-sudo microk8s enable dns dashboard registry
-sudo usermod -a -G microk8s $USER
-sudo chown -f -R $USER ~/.kube
+
+if [ ! -d /home/vagrant/.kube/ ];
+then
+    sudo snap install microk8s --classic --channel=1.18/stable
+    sudo microk8s status --wait-ready
+    sudo microk8s enable dns dashboard registry
+    sudo usermod -a -G microk8s $USER
+    sudo chown -v -f -R $USER ~/.kube
+fi
 
 sudo microk8s kubectl get nodes
 sudo microk8s kubectl get services
 
 # shell in new group
-sudo su -l vagrant
+#sudo su -l vagrant
 
 cat << EOF > /home/vagrant/start-microk8.sh
 #!/bin/bash
@@ -36,3 +41,5 @@ kubectl apply -f ./kubernetes/
 kubectl -n musicplayer get svc
 kubectl -n musicplayer get pods
 EOF
+
+chmod +x /home/vagrant/start-microk8.sh
