e7707b5fbc60949d9f24a8a13db50717905a9368
craig <craig.martin@cloud212.com.au>
2020-10-10 21:15:02 +1100

    fixes

--- container-haproxy/lua/healthping.lua
+++ container-haproxy/lua/healthping.lua
@@ -3,6 +3,8 @@
 function lua_ping_server(applet)
     local response = "ha-pong\n"
     applet:set_status(200)
+    applet:add_header("content-length", string.len(response))
+    applet:add_header("content-type", "text/html")
     applet:start_response()
     applet:send(response)
 end
--- container-haproxy/lua/start.lua
+++ container-haproxy/lua/start.lua
@@ -1,1 +1,3 @@
+-- MPContainer start.lua
+
 core.Debug("HAProxy MPContainer starting\n")
